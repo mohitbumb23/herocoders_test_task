d65543d592ac5a17834720ed754a09e3ed2468b7
Mohit <mohitbumb23@gmail.com>
2021-03-26 21:43:09 +0530

    Updated -ve test case for calculating circles area

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 - Write a JS function that determines the area of a circle given the radius.
 - Write a JS function that determines if a string starts with an upper-case letter A-Z and returns true or false.
 
+## Setup
+
+`npm install`
+
 ## Run
 
 `node index.js` / `npm run start`
--- tests/circle.test.js
+++ tests/circle.test.js
@@ -15,4 +15,13 @@ describe("Testcase for circle", () => {
     const area = circleInstance.area();
     assert.equal(area, computedArea);
   });
+
+  it("Throw error while calculating area of circle when invalid string provided", () => {
+    try {
+      const radius = "abcd";
+      const circleInstance = new Circle(radius);
+    } catch (err) {
+      assert.equal("Invalid radius value for the circle", err.message);
+    }
+  });
 });
